0f5f509cab752d6c17206f299bcae9f841b8a4b3
aricamargo_meli <dario.camargo@mercadolivre.com>
2024-04-21 19:23:13 -0300

    Feature: Create user implementation with jquery

---
+++ assets/js/cadastro.js
@@ -0,0 +1,28 @@
+$('#formulario-cadastro').on('submit', criarUsuario);
+
+function criarUsuario(evento) {
+    evento.preventDefault();
+    console.log("usuario criado");
+
+    if ($('#senha').val() != $('#confirmar-senha').val()) {
+        alert("As senhas não coincidem");
+        return;
+    }
+
+    $.ajax({
+        url: "/usuarios",
+        method: "POST",
+        data: {
+            nome: $('#nome').val(),
+            nick: $('#nick').val(),
+            email: $('#email').val(),
+            senha: $('#senha').val()
+        }
+    }).done(function(){
+        alert("Usuario cadastrado com sucesso");
+    }).fail(function(erro) {
+        console.log(erro);
+        alert("Erro ao cadastrar usuário");
+
+    });
+}
---
+++ src/controllers/usuarios.go
@@ -0,0 +1,37 @@
+package controllers
+
+import (
+	"app/src/respostas"
+	"bytes"
+	"encoding/json"
+	"log"
+	"net/http"
+)
+
+func CriarUsuario(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+	usuario, err := json.Marshal(map[string]string{
+		"nome":  r.FormValue("nome"),
+		"nick":  r.FormValue("nick"),
+		"email": r.FormValue("email"),
+		"senha": r.FormValue("senha"),
+	})
+	if err != nil {
+		respostas.JSON(w, http.StatusBadRequest, respostas.ErroAPI{Erro: err.Error()})
+		return
+	}
+
+	response, err := http.Post("http://localhost:8080/usuarios", "application/json", bytes.NewBuffer(usuario))
+	if err != nil {
+		respostas.JSON(w, http.StatusInternalServerError, respostas.ErroAPI{Erro: err.Error()})
+		return
+	}
+	defer response.Body.Close()
+
+	if response.StatusCode >= 400 {
+		respostas.TratarStatusCodeDeErro(w, response)
+		return
+	}
+
+	respostas.JSON(w, response.StatusCode, nil)
+}
